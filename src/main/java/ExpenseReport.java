6d2becb24f7e40b45ea3b6cdcd2dac14593c4213
basel.obied <basel.obied@fawry.com>
2025-12-30 18:20:37 +0200

    ex: get expanse name by type

--- src/main/java/ExpenseReport.java
+++ src/main/java/ExpenseReport.java
@@ -22,18 +22,7 @@ public class ExpenseReport {
                 mealExpenses += expense.amount;
             }
 
-            String expenseName = "";
-            switch (expense.type) {
-                case DINNER:
-                    expenseName = "Dinner";
-                    break;
-                case BREAKFAST:
-                    expenseName = "Breakfast";
-                    break;
-                case CAR_RENTAL:
-                    expenseName = "Car Rental";
-                    break;
-            }
+            String expenseName = getExpenseName(expense);
 
             String mealOverExpensesMarker = expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 ? "X" : " ";
 
@@ -44,6 +33,14 @@ public class ExpenseReport {
 
         System.out.println("Meal expenses: " + mealExpenses);
         System.out.println("Total expenses: " + total);
+    }
+
+    private static String getExpenseName(Expense expense) {
+        return switch (expense.type) {
+            case DINNER -> "Dinner";
+            case BREAKFAST -> "Breakfast";
+            case CAR_RENTAL -> "Car Rental";
+        };
     }
 
     private static boolean isExpenseMeal(Expense expense) {
