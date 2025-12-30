3068894e86c8d6f874d50fd3dcdebe07cf72cae3
basel.obied <basel.obied@fawry.com>
2025-12-30 18:28:01 +0200

    ex: calculateExpensesMeals from the print loop

--- src/main/java/ExpenseReport.java
+++ src/main/java/ExpenseReport.java
@@ -19,6 +19,9 @@ public class ExpenseReport {
 
         for (Expense expense : expenses) {
             mealExpenses = calculateExpensesMeals(expense, mealExpenses);
+        }
+
+        for (Expense expense : expenses) {
 
             String expenseName = getExpenseName(expense);
 
